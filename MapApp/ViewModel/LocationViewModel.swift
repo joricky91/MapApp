383f19d18a4e7078604374938a5981c318968aab
joricky91 <jonathanricky91@gmail.com>
2024-05-24 16:24:48 +0700

    feat: list locations and changing location

--- MapApp/ViewModel/LocationViewModel.swift
+++ MapApp/ViewModel/LocationViewModel.swift
@@ -10,16 +10,22 @@ import MapKit
 
 class LocationViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location] = []
     
+    // Current location on map
     @Published var mapLocation: Location {
         didSet {
             updateMapRegion(location: mapLocation)
         }
     }
     
+    // Current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -34,6 +40,19 @@ class LocationViewModel: ObservableObject {
             mapRegion = MKCoordinateRegion(
                 center: location.coordinates,
                 span: mapSpan)
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
         }
     }
     
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -15,7 +15,46 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $viewModel.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+            }
         }
+    }
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button {
+                viewModel.toggleLocationsList()
+            } label: {
+                Text(viewModel.mapLocation.name + ", " + viewModel.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.black)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: viewModel.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundStyle(.black)
+                            .padding()
+                            .rotationEffect(Angle(degrees: viewModel.showLocationsList ? 180 : 0))
+                    }
+            }
+            
+            if viewModel.showLocationsList {
+                LocationListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
 
